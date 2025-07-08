3fb9550ae1b84120ffe74ee6b30ecf6194676ace
matusadamovic <mat.adamovic@gmail.com>
2025-07-08 20:15:20 +0200

    new

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,40 @@ const config: Config = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {
+      // tvoje custom fonty
       fontFamily: {
         sans: ['var(--font-geist-sans)', 'sans-serif'],
         mono: ['var(--font-geist-mono)', 'monospace'],
+      },
+      // pridáme animácie
+      animation: {
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
+      },
+      // a k nim keyframes
+      keyframes: {
+        moveHorizontal: {
+          "0%":   { transform: "translateX(-50%) translateY(-10%)" },
+          "50%":  { transform: "translateX(50%) translateY(10%)" },
+          "100%": { transform: "translateX(-50%) translateY(-10%)" },
+        },
+        moveInCircle: {
+          "0%":   { transform: "rotate(0deg)" },
+          "50%":  { transform: "rotate(180deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        moveVertical: {
+          "0%":   { transform: "translateY(-50%)" },
+          "50%":  { transform: "translateY(50%)" },
+          "100%": { transform: "translateY(-50%)" },
+        },
       },
     },
   },
   plugins: [],
 }
+
 export default config
